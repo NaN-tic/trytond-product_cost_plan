10f68f4d851741909ea28096aa53b61083cd358a
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:36 +0200

    Update test to version 4.0

--- tests/test_product_cost_plan.py
+++ tests/test_product_cost_plan.py
@@ -4,33 +4,27 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_cost_plan')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_cost_plan')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_cost_plan'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_product_cost_plan.rst',
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_product_cost_plan.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
-    suite.addTests(
-        doctest.DocFileSuite('scenario_product_cost_plan_extras_depend.rst',
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_product_cost_plan_extras_depend.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
