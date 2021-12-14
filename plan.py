51391273dc462b0de17a9a816db2baf11f846f09
Àngel Àlvarez Serra <angel@nan-tic.com>
2021-12-14 17:04:09 +0100

    avoid recursion exception (#1)
    #044450 avoid recursion exception

--- plan.py
+++ plan.py
@@ -145,7 +145,8 @@ class Plan(DeactivableMixin, ModelSQL, ModelView):
             if input_.product.boms:
                 product_bom = input_.product.boms[0].bom
                 res.append((input_.product.id, product_bom.id))
-                res += self.find_boms(product_bom.inputs)
+                if product_bom.id not in (x[1] for x in res):
+                    res += self.find_boms(product_bom.inputs)
         return res
 
     @fields.depends('bom', 'boms', 'product')
