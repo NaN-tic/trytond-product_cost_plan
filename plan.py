8ef6fd12a83df3a96ca110969b7ad14292c1ffa1
Albert Cervera i Areny <albert@nan-tic.com>
2023-08-11 17:27:07 +0200

    Remove pdb.set_trace()

--- plan.py
+++ plan.py
@@ -149,7 +149,6 @@ class Plan(DeactivableMixin, ModelSQL, ModelView):
 
     @fields.depends('bom', 'boms', 'product')
     def on_change_with_boms(self):
-        import pdb; pdb.set_trace()
         boms = {
             'delete': [x.id for x in self.boms],
             'add': [],
